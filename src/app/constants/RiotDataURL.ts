ba50e19e61d6dcdd28b3982bfc72318fb4725ff2
JinChaeKim <rlawlsco88@gmail.com>
2025-03-13 14:51:14 +0900

    Feat : #8 동적 버전 적용한 API

---
+++ src/app/constants/RiotDataURL.ts
@@ -0,0 +1,19 @@
+import { getLatestVersion } from "./GetVersion";
+
+// 챔피언 목록 API
+export const CHAMPION_LIST_URL = async (): Promise<string> => {
+  const version = await getLatestVersion();
+  return `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`;
+};
+
+// 특정 챔피언 상세 정보 API
+export const PICK_CHAMPION_URL = async (): Promise<string> => {
+  const version = await getLatestVersion();
+  return `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion/{id}.json`;
+};
+
+// 아이템 목록 API
+export const LOL_ITEM_URL = async (): Promise<string> => {
+  const version = await getLatestVersion();
+  return `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/item.json`;
+};
