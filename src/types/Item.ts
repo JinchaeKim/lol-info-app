0230de6e9ed3adc97448869288705d94ad76c5af
JinChaeKim <rlawlsco88@gmail.com>
2025-03-14 15:18:52 +0900

    refactor : #3 아이템 타입 수정

--- src/types/Item.ts
+++ src/types/Item.ts
@@ -1,12 +1,9 @@
+export type ItemResponse = Record<string, Item>;
+
 export type Item = {
   name: string;
   plaintext: string;
   image: { full: string };
   gold: { total: number };
   stats: { FlatMovementSpeedMod: number };
-};
-
-// 동적 key (1001, 1002...)
-export type ItemResponse = {
-  data: { [key: string]: Item };
 };
