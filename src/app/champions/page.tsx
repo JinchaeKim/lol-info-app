71ff9d71cc104b1d4652777cc54c4410a5719088
JinChaeKim <rlawlsco88@gmail.com>
2025-03-14 15:21:01 +0900

    feat : #5 챔피언 목록 페이지

--- src/app/champions/page.tsx
+++ src/app/champions/page.tsx
@@ -1,7 +1,34 @@
+import { fetchChampionList } from "@/utils/server-action";
 import React from "react";
+import { CHAMPION_IMG_URL } from "../constants/RiotDataURL";
+import Image from "next/image";
 
-const ChampionsPage = () => {
-  return <h1 className="flexCenter title">Champion List</h1>;
+const ChampionsPage = async () => {
+  const imgUrl = await CHAMPION_IMG_URL();
+  const data = await fetchChampionList();
+  // console.log("data", data);
+  return (
+    <main className="m-[50px]">
+      <h1 className="flexCenter title mt-[80px] text-[30px]">Champion List</h1>
+      <div className="itemGrid mt-[30px] auto-rows-[minmax(200px,auto)]">
+        {data.map((champion) => {
+          return (
+            <div key={champion.key} className="itemBorder h-auto w-[200px] p-4">
+              <Image
+                src={`${imgUrl}${champion.image.full}`}
+                alt="Picture of the Champion"
+                width={100}
+                height={100}
+                className="mx-auto flex"
+              />
+              <h2 className="title mt-2 text-[20px]">{champion.name}</h2>
+              <p className="text-emerald-50">{champion.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
 };
 
 export default ChampionsPage;
