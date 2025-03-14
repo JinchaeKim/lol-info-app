3c1918a2297f07ba661ba98565399374be75fe16
JinChaeKim <rlawlsco88@gmail.com>
2025-03-14 15:15:32 +0900

    refactor : #4 정규표현식-태그 및 @ 지우기

--- src/utils/server-action.ts
+++ src/utils/server-action.ts
@@ -6,31 +6,46 @@ import {
   PICK_CHAMPION_URL,
 } from "@/app/constants/RiotDataURL";
 import { Champions } from "@/types/Champion";
-import { Item, ItemResponse } from "@/types/Item";
+import { Item } from "@/types/Item";
+import { formatValue } from "./formatValue";
 
 // 챔피언 목록 패칭
 export async function fetchChampionList(): Promise<Champions[]> {
   const chamUrl = await CHAMPION_LIST_URL();
-  const res = await fetch(chamUrl); //fetch의 첫번째 인자는 문자열
-  const data: Champions = await res.json();
+  const res = await fetch(chamUrl, { next: { revalidate: 86400 } }); //fetch의 첫번째 인자는 문자열
+  const { data } = await res.json(); // 전체 객체 중 data만
+  console.log("data", data);
 
-  return [data];
+  const a = Object.values(data);
+  console.log("a", a);
+  return Object.values(data); // data를 배열로 변환
 }
 
 // 특정 챔피언 상세 정보 패칭
 export async function fetchPickChampionList(): Promise<Champions[]> {
   const pickUrl = await PICK_CHAMPION_URL();
-  const res = await fetch(pickUrl, { next: { revalidate: 86400 } }); //fetch의 첫번째 인자는 문자열
-  const resData: Champions = await res.json();
+  const res = await fetch(pickUrl); //fetch의 첫번째 인자는 문자열
+  const data: Champions = await res.json();
 
-  return Object.entries(resData.data);
+  return [data];
 }
 
 // 아이템 데이터 패칭
 export async function fetchItemList(): Promise<[string, Item][]> {
   const itemUrl = await LOL_ITEM_URL();
   const res = await fetch(itemUrl, { cache: "force-cache" }); //fetch의 첫번째 인자는 문자열
-  const resData: ItemResponse = await res.json(); //동적 key
+  const { data } = await res.json();
+  // 데이터 정제
+  const cleanedData = Object.entries(data).map(([key, item]) => {
+    return [
+      key,
+      {
+        ...item,
+        name: formatValue(item.name),
+        plaintext: formatValue(item.plaintext),
+      },
+    ];
+  });
 
-  return Object.entries(resData.data); //배열로 반환
+  return cleanedData as [string, Item][]; // 타입 캐스팅
 }
