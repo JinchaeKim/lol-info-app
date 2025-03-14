3fdd44f3e0681d0bff5675ddd495ef90a56d436e
JinChaeKim <rlawlsco88@gmail.com>
2025-03-14 15:23:19 +0900

    feat : #9 홈 페이지 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
     <>
-      <h1 className="flexCenter title mt-[50px]">LEAGUE of LEGENDS info App</h1>
-      <p className="flexCenter text-slate-400 font-bold my-[15px]">
+      <h1 className="flexCenter title mt-[50px] text-[30px]">
+        LEAGUE of LEGENDS info App
+      </h1>
+      <p className="flexCenter my-[15px] font-bold text-slate-400">
         Riot Games API를 활용하여 챔피언과 아이템 정보를 제공합니다.
       </p>
     </>
