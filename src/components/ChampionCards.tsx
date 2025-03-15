8d15b62675436dfc6182aa3a3fa2dcedb8a24683
JinChaeKim <rlawlsco88@gmail.com>
2025-03-15 13:17:15 +0900

    refactor : #5 카드 컴포넌트 분리

---
+++ src/components/ChampionCards.tsx
@@ -0,0 +1,32 @@
+import { CHAMPION_IMG_URL } from "@/app/constants/RiotDataURL";
+import { Champions } from "@/types/Champion";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Cards = async ({ data }: { data: Champions[] }) => {
+  const img_Url = await CHAMPION_IMG_URL();
+  return (
+    <div className="itemGrid mt-[30px] auto-rows-[minmax(200px,auto)]">
+      {data.map((champion) => {
+        return (
+          <Link key={champion.key} href={`/champions/${champion.id}`}>
+            <div className="itemBorder h-auto w-[200px] p-4">
+              <Image
+                src={`${img_Url}${champion.image.full}`}
+                alt="Picture of the Champion"
+                width={100}
+                height={100}
+                className="mx-auto flex"
+              />
+              <h2 className="title mt-2 text-[20px]">{champion.name}</h2>
+              <p className="text-emerald-50">{champion.title}</p>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cards;
