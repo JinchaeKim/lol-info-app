11f78ff0648f186e837908952e78794bee082914
JinChaeKim <rlawlsco88@gmail.com>
2025-03-17 23:04:49 +0900

    feat : #10 카드 컴포넌트 구성

---
+++ src/_components/Card.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState } from "react";
+
+type CardProps = {
+  id: string | null;
+  name: string;
+  title: string;
+  img_Url: string;
+  image: { full: string };
+};
+
+const Card: React.FC<CardProps> = ({ id, name, title, img_Url, image }) => {
+  const [hover, setHover] = useState<string | null>(null);
+
+  const CardContens = (
+    <div
+      className="hoverAction h-auto w-[200px] p-4 shadow-xl"
+      onMouseEnter={() => setHover(id)}
+      onMouseLeave={() => setHover(null)}
+    >
+      <Image
+        src={`${img_Url}${image.full}`}
+        alt="Picture of the Champion"
+        width={200}
+        height={200}
+        className="rounded-lg"
+      />
+      {hover === id && (
+        <div className="flexCenter hoverCard">
+          <div className="flexCenter z-20 flex-col">
+            <h2 className="title text-[25px]">
+              {name.replace(/<[^>]+>|@\w+@/g, "")}
+            </h2>
+            <p className="text-emerald-50">
+              {title.replace(/<[^>]+>|@\w+@/g, "")}
+            </p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+  return (
+    <>
+      {id ? (
+        <Link className="mx-auto flex" href={`/champions/${id}`}>
+          {CardContens}
+        </Link>
+      ) : (
+        CardContens
+      )}
+    </>
+  );
+};
+
+export default Card;
