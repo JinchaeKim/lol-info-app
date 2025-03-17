fcad2029f1738fc74b882fbbd25b937991c90a34
JinChaeKim <rlawlsco88@gmail.com>
2025-03-18 01:52:49 +0900

    refactor : #8 라우트 핸들러 에러 처리

--- src/app/api/rotation/route.ts
+++ src/app/api/rotation/route.ts
@@ -1,23 +1,41 @@
 import { Champions } from "@/types/Champion";
 import { ChampionRotation } from "@/types/ChampionRotation";
-import { fetchChampionList } from "@/utils/server-action";
+import { fetchChampionList } from "@/utils/serverApi";
 import { NextResponse } from "next/server";
 
+// 로테이션 API 라우트 핸들러 + 필터
 export const GET = async (): Promise<NextResponse> => {
-  //return 값 타입 지정 후 적용
-  const res = await fetch(
-    "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
-    {
-      headers: {
-        "X-Riot-Token": process.env.NEXT_PUBLIC_RIOT_API_KEY!,
+  try {
+    const res = await fetch(
+      "https://kr.api.riotgames.com/lol/platform/v3/champion-rotations",
+      {
+        headers: {
+          "X-Riot-Token": process.env.NEXT_PUBLIC_RIOT_API_KEY!,
+        },
       },
-    },
-  );
-  // 필터
-  const rotationData: ChampionRotation = await res.json(); // freeChampionIds 배열 포함 객체
-  const championData: Champions[] = await fetchChampionList(); // 모든 챔피언 리스트 배열
-  const filterData: Champions[] = championData.filter((el) => {
-    return rotationData.freeChampionIds.includes(parseInt(el.key));
-  }); // 챔피언 키를 포함한 로테이션 아이디를 챔피언 리스트에서 filter
-  return NextResponse.json(filterData);
+    );
+
+    // 사용자 요청 에러
+    if (!res) {
+      return NextResponse.json(
+        { message: "error! 로테이션 챔피언 정보를 찾을 수 없습니다!" },
+        { status: 404 },
+      );
+    }
+
+    // 필터
+    const rotationData: ChampionRotation = await res.json(); // freeChampionIds 배열 포함 객체
+    const championData: Champions[] = await fetchChampionList(); // 모든 챔피언 리스트 배열
+    const filterData: Champions[] = championData.filter((el) => {
+      return rotationData.freeChampionIds.includes(parseInt(el.key));
+    }); // 챔피언 키를 포함한 로테이션 아이디를 챔피언 리스트에서 filter
+    return NextResponse.json(filterData, { status: 200 });
+  } catch (error) {
+    console.log("로테이션 라우트 핸들러 에러 발생", error);
+    // 서버 에러
+    return NextResponse.json(
+      { message: "error! 로테이션 페이지를 읽을 수 없습니다!" },
+      { status: 500 },
+    );
+  }
 };
--- src/app/api/versions/route.ts
+++ src/app/api/versions/route.ts
@@ -1,10 +1,28 @@
 import { NextResponse } from "next/server";
 
+// API 버전 라우트 핸들러
 export const GET = async (): Promise<NextResponse> => {
-  const res = await fetch(
-    "https://ddragon.leagueoflegends.com/api/versions.json",
-    { next: { revalidate: 86400 } },
-  );
-  const data: string[] = await res.json();
-  return NextResponse.json(data[0]);
+  try {
+    const res = await fetch(
+      "https://ddragon.leagueoflegends.com/api/versions.json",
+      { next: { revalidate: 86400 } },
+    );
+
+    // 사용자 요청 에러
+    if (!res) {
+      return NextResponse.json(
+        { message: "error! 버전 정보를 찾을 수 없습니다!" },
+        { status: 400 },
+      );
+    }
+    const data: string[] = await res.json();
+    return NextResponse.json(data[0], { status: 200 });
+  } catch (error) {
+    console.log("버전 라우트 핸들러 에러 발생!", error);
+    // 서버 에러
+    return NextResponse.json(
+      { message: "error! 버전 정보를 읽을 수 없습니다! " },
+      { status: 500 },
+    );
+  }
 };
