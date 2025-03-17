3dc059175c0580a34fa458e286eb783976fc0f71
JinChaeKim <rlawlsco88@gmail.com>
2025-03-17 10:08:52 +0900

    remove : 버전 정보 API 삭제

--- src/app/constants/GetVersion.ts
+++
@@ -1,8 +0,0 @@
-// API 버전 정보 가져오기 (요청 때마다 직접 라이엇 API)
-// export const getLatestVersion = async (): Promise<string> => {
-//   const res = await fetch(
-//     "https://ddragon.leagueoflegends.com/api/versions.json",
-//   );
-//   const versions: string[] = await res.json();
-//   return versions[0];
-// };
