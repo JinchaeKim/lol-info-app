c5663b5bfff635ed16ef2dc2e8f97473bdf3bdbc
JinChaeKim <rlawlsco88@gmail.com>
2025-03-18 09:59:54 +0900

    refactor : 버전 url 상수화

--- src/app/constants/RiotDataURL.ts
+++ src/app/constants/RiotDataURL.ts
@@ -1,10 +1,11 @@
 import { getVersion } from "@/utils/riotApi";
+import { BASE_URL } from "./BaseUrl";
 // import { getLatestVersion } from "./GetVersion";
 
 // 챔피언 목록 API
 export const CHAMPION_LIST_URL = async () => {
   const version = await getVersion();
-  return `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion.json`;
+  return `${BASE_URL}${version}/data/ko_KR/champion.json`;
 };
 
 // 특정 챔피언 상세 정보 API
@@ -14,23 +15,23 @@ export const PICK_CHAMPION_URL = async ({
   id: string;
 }): Promise<string> => {
   const version = await getVersion();
-  return `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/champion/${id}.json`;
+  return `${BASE_URL}${version}/data/ko_KR/champion/${id}.json`;
 };
 
 // 아이템 목록 API
 export const LOL_ITEM_URL = async (): Promise<string> => {
   const version = await getVersion();
-  return `https://ddragon.leagueoflegends.com/cdn/${version}/data/ko_KR/item.json`;
+  return `${BASE_URL}${version}/data/ko_KR/item.json`;
 };
 
 // 아이템 이미지 API
 export const ITEM_IMG_URL = async (): Promise<string> => {
   const version = await getVersion();
-  return `https://ddragon.leagueoflegends.com/cdn/${version}/img/item/`;
+  return `${BASE_URL}${version}/img/item/`;
 };
 
 // 챔피언 이미지 API
 export const CHAMPION_IMG_URL = async (): Promise<string> => {
   const version = await getVersion();
-  return `https://ddragon.leagueoflegends.com/cdn/${version}/img/champion/`;
+  return `${BASE_URL}${version}/img/champion/`;
 };
