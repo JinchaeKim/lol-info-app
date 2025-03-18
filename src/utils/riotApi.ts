53b202574b66707d22a4864fc88e35192b3e1b76
JinChaeKim <rlawlsco88@gmail.com>
2025-03-18 12:55:11 +0900

    chore : 주석 추가

--- src/utils/riotApi.ts
+++ src/utils/riotApi.ts
@@ -1,5 +1,6 @@
 import { Champions } from "@/types/Champion";
 
+// 로테이션 라우트 핸들러 응답
 export const getChampionRotation = async (): Promise<Champions[]> => {
   const res = await fetch("http://localhost:3000/api/rotation");
   const data = await res.json();
@@ -7,6 +8,7 @@ export const getChampionRotation = async (): Promise<Champions[]> => {
   return data;
 };
 
+// 버전 라우트 핸들러 응답
 export const getVersion = async (): Promise<string[]> => {
   const res = await fetch("http://localhost:3000/api/versions");
   const data = await res.json();
